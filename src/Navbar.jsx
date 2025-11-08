7407a5ae28febd834bab13e11e54987325dc5099
Jassboy1234 <135118640+Jassboy1234@users.noreply.github.com>
2025-11-08 19:00:46 +0300

    Add files via upload

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -6,8 +6,8 @@ const Navbar = () => {
     <nav className="navbar-box">
       <h1 className="logo">Infinimod</h1>
       <ul className="nav-links">
-        <li href="/">Home</li>
-        <li href="/features">Features</li>
+        <button href="/">Home</button>
+        <button href="/features">Features</button>
       </ul>
     </nav>
   );
